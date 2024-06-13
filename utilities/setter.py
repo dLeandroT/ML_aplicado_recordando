e7d35ed6470b489a0ce325c70260d85d13d9acdd
Leandro Tenjo <code@leandrot.com>
2024-06-12 20:54:20 -0500

    Integracion dfe variables de entorno

---
+++ utilities/setter.py
@@ -0,0 +1,14 @@
+import os
+from base64 import b64decode
+
+
+def main():
+    key = os.environ.get('GDRIVE_CREDENTIALS_DATA')
+    with open('path.json', 'w') as json_file:
+        json_file.write(b64decode(key).decode())
+
+    print(os.path.realpath('path.json'))
+
+
+if __name__ == '__main__':
+    main()
