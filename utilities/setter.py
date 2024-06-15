126d470e3a051de800964d5ceea2fa62e6824a51
Leandro Tenjo <code@leandrot.com>
2024-06-14 20:31:09 -0500

    Mejora en manejo de errores

--- utilities/setter.py
+++ utilities/setter.py
@@ -1,14 +1,20 @@
 import os
 from base64 import b64decode
 
-
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
+    if key is None:
+        raise ValueError("Environment variable 'SERVICE_ACCOUNT_KEY' not found.")
+    
+    try:
+        decoded_key = b64decode(key).decode()
+    except Exception as e:
+        raise ValueError("Failed to decode the key from base64.") from e
+    
     with open('path.json', 'w') as json_file:
-        json_file.write(b64decode(key).decode())
+        json_file.write(decoded_key)
 
     print(os.path.realpath('path.json'))
-
 
 if __name__ == '__main__':
     main()
